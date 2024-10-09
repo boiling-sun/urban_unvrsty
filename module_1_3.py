f15d9e64a14c65432a0b9cf41c0157fc2a7be888
Anna Prokofeva <142903268+boiling-sun@users.noreply.github.com>
2024-10-09 18:00:08 +0300

    style module_1_3.py

--- module_1_3.py
+++ module_1_3.py
@@ -3,9 +3,9 @@ def main():
     age = 31
     new_age = age + 1
     is_student = True
-    print(f'Name: {name}\n'+
-          f'Age: {age}\n'+
-          f'New Age: {new_age}\n'+
+    print(f'Name: {name}\n' +
+          f'Age: {age}\n' +
+          f'New Age: {new_age}\n' +
           f'Is student: {is_student}')
 
 if __name__ == '__main__':
