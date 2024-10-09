6de515abcbcff0e582ea5883cbc3485da06274cb
Anna Prokofeva <142903268+boiling-sun@users.noreply.github.com>
2024-10-09 17:55:32 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,12 @@
+def main():
+    name = 'Anna'
+    age = 31
+    new_age = age + 1
+    is_student = bool(1)
+    print(f'Name: {name}\n'+
+          f'Age: {age}\n'+
+          f'New Age: {new_age}\n'+
+          f'Is student: {is_student}')
+
+if __name__ == '__main__':
+    main()
