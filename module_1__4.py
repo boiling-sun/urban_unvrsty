f15326aa26796ad4997e720bc472a67d3dce0647
Anna Prokofeva <142903268+boiling-sun@users.noreply.github.com>
2024-10-09 19:14:42 +0300

    Create module_1__4.py

---
+++ module_1__4.py
@@ -0,0 +1,13 @@
+def main():
+    my_string = input('Enter text: ')
+    print(len(my_string))
+    print(my_string.upper())
+    print(my_string.lower())
+    print(my_string.replace(' ', ''))
+    print(my_string[0])
+    print(my_string[-1])
+
+
+if __name__ == '__main__':
+    main()
+    
