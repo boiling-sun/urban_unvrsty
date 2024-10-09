adb77210f8adf3188d6709c3b0341d803bda14f7
Anna Prokofeva <142903268+boiling-sun@users.noreply.github.com>
2024-10-09 18:55:55 +0300

    Create module_1_5.py

---
+++ module_1_5.py
@@ -0,0 +1,10 @@
+def main():
+    example = 'Hello, world'
+    print(example[0])
+    print(example[-1])
+    print(example[len(example)//2:])
+    print(example[::-1])
+    print(example[1::2])
+
+if __name__ == "__main__":
+    main()
