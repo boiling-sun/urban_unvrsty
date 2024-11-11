b609c3eade733a420c6e8828623e5a8f5bf37bf0
Anna Prokofeva <boiling.sun@icloud.com>
2024-11-11 19:08:22 +0300

    homework 4.2 (namespaces)

---
+++ module_4/module_4_2.py
@@ -0,0 +1,17 @@
+"""
+Ваша задача:
+Создайте новую функцию test_function
+Создайте внутри test_function другую функцию - inner_function, 
+Эта функция должна печатать значение "Я в области видимости функции test_function"
+Вызовите функцию inner_function внутри функции test_function
+Попробуйте вызывать inner_function вне функции test_function и посмотрите на результат выполнения программы
+"""
+
+def test_function():
+    def inner_function():
+        print("Я в области видимости функции test_function")
+    inner_function()
+
+if __name__ == '__main__':
+    test_function()
+    #inner_function() # "NameError: name 'inner_function' is not defined. Did you mean: 'test_function'?"
